3bab4990a0333ef4abea5671903e5a2771500556
haidarniv <haidarapps12@gmail.com>
2025-04-13 11:55:09 +0700

    re

--- index.mjs
+++ index.mjs
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "docs")));
 app.use(express.json());
 
 app.post("/generate", async (req, res) => {
